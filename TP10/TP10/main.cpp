b04faa888bf60015f7f6fbe97e31195015d5763f
Alan <31071359+amechu@users.noreply.github.com>
2018-05-25 13:01:39 -0300

    comete este neperiano

--- TP10/TP10/main.cpp
+++ TP10/TP10/main.cpp
@@ -12,43 +12,40 @@
 
 #define MY_LCD_DESCRIPTION "EDA LCD 3 B"
 
-int main(void)
+int main(int argc, char** argv)
 {
-	Hitachi hitachi(MY_LCD_DESCRIPTION);
-	if (hitachi.lcdInitOk()) {
+	if (argc == 2) {
 
-
-	//Presentacion
-		hitachi << (const unsigned char *)"    LOADING..    " ;
-		for (int i = 0; i < 13; i++) {
-			hitachi << (char)0xFF;
-			hitachi.wait(50);
+		std::string rss = getRss(argv[1]);
+		if (rss != "") {
+			Feed feed;
+			feed.loadInfo(rss); //aca esta expat y fsm
 		}
-		hitachi.sendByte(RS::DATA_REGISTER, 0xFF);
-		hitachi.wait(700);
-		hitachi.lcdClear();
-		hitachi << (const unsigned char*)"     READY!     ";
-		hitachi.wait(400);
-		hitachi.lcdClear();
-
-	//Mini tecladito
-		char ch = NULL;
-		while ( (ch = getchar() ) != '}') //Esto es un teclado con el cual podemos probar el display char a char. Con { se limpia la pantalla y con } se cierra el programa.
-		{
-			if(ch != '\n')
-				hitachi << ch;
-
-			if (ch == '{')
-				hitachi.lcdClear();
+		else {
+			std::cout << "Could not load RSS from host: " << argv[1] << std::endl;
+			std::cout << "Check if the host is correct, or if you are connected to the internet." << std::endl;
+			getchar();
 		}
+		Hitachi hitachi(MY_LCD_DESCRIPTION);
+		if (hitachi.lcdInitOk()) {
+			hitachi.show(); //aca mostramos las cosas
+		}
+		else {
+			std::cout << "Could not load LCD Hitachi: " << MY_LCD_DESCRIPTION << std::endl;
+			std::cout << "Check if the LCD description is right, or if the LCD is plugged in." << std::endl;
+			getchar();
+		}
+		return 0;
 	}
-
-	//Error
-	else {
-		std::cout << "Could not load LCD Hitachi: " << MY_LCD_DESCRIPTION << std::endl;
-		std::cout << "Check if the LCD description is right, or if the LCD is plugged in." << std::endl;
-		getchar();
-	}
-
-	return 0;
 }
+
+
+//char ch = NULL;
+//while ( (ch = getchar() ) != '}') //Esto es un teclado con el cual podemos probar el display char a char. Con { se limpia la pantalla y con } se cierra el programa.
+//{
+//	if(ch != '\n')
+//		hitachi << ch;
+
+//	if (ch == '{')
+//		hitachi.lcdClear();
+//}
