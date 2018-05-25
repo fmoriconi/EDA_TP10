7c50a445c750baba14e2440f6f38c29f52ed2879
fmoriconi <31166325+fmoriconi@users.noreply.github.com>
2018-05-25 16:35:35 -0300

    tp choto

--- TP10/TP10/XML.h
+++ TP10/TP10/XML.h
@@ -2,6 +2,13 @@
 
 #include "expat.h"
 #include <iostream>
+#include <string>
+
+typedef myUserData{
+	uint32_t depth;
+	std::string * parElement; ///Este no se si esta bien.
+	char * buffer;
+};
 
 class XML
 {
@@ -9,12 +16,16 @@ public:
 	XML();
 	~XML();
 
+	void parse(FEED feed_, std::string rss_ );
+
+
 	XML_Parser p;
 	XML_CharacterDataHandler chardata;
 	XML_Status status;
 	myUserData UD;
 	FILE * fp;
-	
+
+private:
 
 };
 
