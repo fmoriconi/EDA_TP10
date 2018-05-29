8d606a9f71b1745b9111195e463ae3eb8805a2c1
amartorell <amartorell@itba.edu.ar>
2018-05-28 23:42:11 -0300

    client mejorado

--- tp10eda/tp10eda/client.cpp
+++ tp10eda/tp10eda/client.cpp
@@ -10,22 +10,30 @@ client::client()
 	client_resolver = new boost::asio::ip::tcp::resolver(*IO_handler);
 }
 
+client::client(const char * parser, const char * port)
+{
+	IO_handler = new boost::asio::io_service();
+	socket_forClient = new boost::asio::ip::tcp::socket(*IO_handler);
+	client_resolver = new boost::asio::ip::tcp::resolver(*IO_handler);
+	this->parserline = parser;
+	this->port = port;
+	prepareMessage(parser);
+}
+
 void client::connect(const char * host, const char * port)
 {
 	endpoint = client_resolver->resolve(
 		boost::asio::ip::tcp::resolver::query(host,port ));
 	boost::asio::connect(*socket_forClient, endpoint);
 	socket_forClient->non_blocking(true);
-	
 	std::cout << "conecto" << std::endl;
 }
 
 void client::receiveMessage()
 {
-	//archivo.open("test.xml", ios::binary);
+	
 	ofstream archivo("archivo.xml", ios::binary);
-	//ofstream teste("test2.xml");
-	//archivo.open("test.xml", ios::binary);
+
 	boost::system::error_code error;
 	char buf[0xFFFF];
 	size_t len = 0;
@@ -44,6 +52,7 @@ void client::receiveMessage()
 			elapsedSeconds += (currentTime.wall - pastTime.wall) / 1e9;
 			pastTime = currentTime;
 			cout << "Pasaron " << elapsedSeconds << " segundos." << endl;
+			
 		}
 
 		if (!error)
@@ -51,11 +60,11 @@ void client::receiveMessage()
 	
 		if (!error)
 		{
-		//	std::cout << buf;
+		//	std::cout << buf;	//con este cout si tarda bastante sino no tarda ni un segundo
 			archivo<<buf;
 		//	teste << buf;
 		}
-	} while (elapsedSeconds < 20.0);	//se trata de leer todo el rss en 10 segundos esto se puede cambiar 
+	} while (elapsedSeconds < 10.0);	//se trata de leer todo el rss en 10 segundos esto se puede cambiar 
 
 	//if (!error)
 		//std::cout << std::endl << "Server sais: " << buf;
@@ -79,6 +88,34 @@ void client::sendMessage(const char * msg)
 		std::cout << "Error while trying to connect to server " << error.message() << std::endl;
 }
 
+void client::sendMessage()
+{
+	size_t len;
+	boost::system::error_code error;
+
+	do
+	{
+		len = socket_forClient->write_some(boost::asio::buffer((message.c_str()), strlen((message.c_str()))), error);
+	} while ((error.value() == WSAEWOULDBLOCK));
+	if (error)
+		std::cout << "Error while trying to connect to server " << error.message() << std::endl;
+}
+
+
+void client::prepareMessage(const char * parser)
+{
+	string firstp = "";
+	string secondp = "";
+	stringstream aux(parser);
+	getline(aux, firstp, '/');
+	getline(aux, secondp);
+	host = (firstp);
+	rss = ("/" + secondp);
+	secondp = "GET /" + secondp + " HTTP/1.1\n";				//le pongo la barra ahi despuesdel get pq se borra cuando haces el getline
+	firstp = "Host: " + firstp + "\n";
+	string Final = secondp + firstp + "\n";
+	this->message = Final;
+}
 
 client::~client()
 {
--- tp10eda/tp10eda/client.h
+++ tp10eda/tp10eda/client.h
@@ -5,19 +5,53 @@
 #include <boost/chrono.hpp>
 #include <boost/timer/timer.hpp>
 #include <fstream>
+using namespace std;
 class client
 {
 public:
 	client();
+	client(const char * parser, const char * port);
+
 	void connect(const char* host, const char * port);
-	void receiveMessage();
-	void sendMessage(const char * msg);
-	~client();
+
+	void receiveMessage();//recibe el mensaje de la pagina y lo transcribe bit a bit a un archivo llamado "archivo.xml"
+						//siempre se queda esperando 10 segundos a que llegue toda la informacion
+	void sendMessage(const char * msg);	//con este mando todo el GET HOST y la linea en blanco que sale si haces "\n"(0d 0a)
+										//pasarle todo ese mensaje como un string concatenado de los 3 agregandole un \n despues de HTTP 1.1 y del Host
+	void sendMessage();					//misma funcion pero sobrecargada para mandar el mensage directamento utilizando el dato miembro
+
+
+
+	void prepareMessage(const char * parser); //setea host y rss a partir del parser
+
+
+	//setter y getters por si se desea modificar algo
+
+	void setHost(std::string host_) { this->host = host_; };
+	void setRss(std::string rss_) { this->rss = rss_; };
+	void setPort(const char * port_) { this->port = port_; };
+	void setParser(const char * parser) { this->parserline = parser; };
+
+	string Gethost() { return this->host; };
+	string GetRss() { return this->rss; };
+	string getMessage() { return message; };
+	string getPort() { return port; };
+	string getParser() { return parserline; };
+
+
+
+	~client();							
 
 private:
 	boost::asio::io_service* IO_handler;
 	boost::asio::ip::tcp::socket* socket_forClient;
 	boost::asio::ip::tcp::resolver* client_resolver;
 	boost::asio::ip::tcp::resolver::iterator endpoint;
+	const char * parserline;
+	string host;
+	string rss;
+	const char * port;
+	std::string message;
+	
 };
 
